ae9130833ef57b59edb5c11c8b12e24c28b24941
Van <lly219@gmail.com>
2019-06-04 00:06:06 +0800

    :lock:

--- src/ts/home.ts
+++ src/ts/home.ts
@@ -60,7 +60,7 @@ const init = async () => {
     const syncBtnElement = document.getElementById("syncBtn");
 
     if (location.search.split("=")[1]) {
-        document.querySelector(".header__item--current").className = "header__item";
+        document.querySelector(".header__item--current").className = "header__item left";
         syncBtnElement.innerHTML = "UPDATE";
     } else {
         syncBtnElement.innerHTML = "PUBLISIH";
